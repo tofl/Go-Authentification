7070e350c068814058066b23ea3e2181160b18ca
Tom F <thomas.flitterman@ynov.com>
2019-01-13 15:23:08 +0100

    Added two endpoints : protected & create token

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/mux"
+	"github.com/mitchellh/mapstructure"
+	"log"
+	"net/http"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type JwtToken struct {
+	Token string `json:"token"`
+}
+
+type Exception struct {
+	Message string `json:"message"`
+}
+
+func main() {
+	router := mux.NewRouter()
+	fmt.Println("Starting the application...")
+	router.HandleFunc("/authenticate", CreateTokenEndpoint).Methods("POST")
+	router.HandleFunc("/protected", ProtectedEndpoint).Methods("GET")
+	log.Fatal(http.ListenAndServe(":12345", router))
+}
+
+func CreateTokenEndpoint(w http.ResponseWriter, r *http.Request) {
+	var user User
+	_ = json.NewDecoder(r.Body).Decode(&user)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": user.Username,
+	})
+	tokenString, err := token.SignedString([]byte("secret"))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	json.NewEncoder(w).Encode(JwtToken{Token: tokenString})
+}
+
+func ProtectedEndpoint(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+
+	token, _ := jwt.Parse(params["token"][0], func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("There was an error")
+		}
+		return []byte("secret"), nil
+	})
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		var user User
+
+		mapstructure.Decode(claims, &user)
+		json.NewEncoder(w).Encode(user)
+	} else {
+		json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+	}
+}
